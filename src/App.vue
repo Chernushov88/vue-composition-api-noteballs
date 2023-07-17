e8f4c5c152b2bea06a8041bdd1ced6a2a6559ca2
chernushov88@gmail.com <chernushov88@gmail.com>
2023-07-17 21:03:21 +0100

    Noteballs: Bulma & Design

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,19 @@
 <template>
-  <RouterLink to="/">Notes</RouterLink> | 
-  <RouterLink to="/stats">Stats</RouterLink>
+  <NavBar />
 
-  <RouterView />
+  <div class="container is-max-desktop px-2 py-4">
+    <RouterView />
+  </div>
 </template>
+
+<script setup>
+/*
+  imports
+*/
+import 'bulma/css/bulma.css'
+import NavBar from '@/components/Layout/NavBar.vue'
+</script>
+
+<style>
+
+</style>
